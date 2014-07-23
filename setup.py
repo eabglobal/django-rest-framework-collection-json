8912722903210a7d57f03fbe34f922bccd91c01f
Chris Marinos <chris@chrismarinos.com>
2014-07-23 17:50:44 -0400

    Bump version: 0.0.1_dev → 0.0.1_dev_2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='djangorestframework_collection_json',
-    version='0.0.1_dev',
+    version='0.0.1_dev_2',
     description='Collection+JSON support for Django REST Framework',
     author='Advisory Board Company',
     packages=find_packages(exclude=['']),
