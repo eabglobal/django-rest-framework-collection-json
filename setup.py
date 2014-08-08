28e615f8a99dc2aa0d178266e9196ccfadc55ea9
Chris Marinos <chris@chrismarinos.com>
2014-08-08 13:30:50 -0400

    fix underscores in package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='djangorestframework_collection_json',
+    name='djangorestframework-collection-json',
     version='0.0.1_dev_2',
     description='Collection+JSON support for Django REST Framework',
     author='Advisory Board Company',
